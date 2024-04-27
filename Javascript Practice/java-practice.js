7b6314bf90e20794a87bb7a27cea47fa5d50907a
brevinlay <lay18007@byui.edu>
2024-04-27 11:23:11 -0700

    update

---
+++ Javascript Practice/java-practice.js
@@ -0,0 +1,7 @@
+const myApples = 4;
+const friendApples = 2;
+let total = myApples + friendApples;
+
+document.getElementById("myApple").textContent = myApples;
+document.getElementById("friendApple").textContent = friendApples;
+document.getElementById("totalApples").textContent = total;
